5151eb50f307b75cc00b9d0463ac70cc6c156f50
fxrar <khatookebhakt+@gmail.com>
2026-02-13 20:39:58 +0530

    feat: initial vite react project with tailwind and custom config

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      <p>Edit to see changes in the browser!</p>
+    
+    </>
+  )
+}
+
+export default App
